9524f581f4eb80356d3b5f937c83c87ee3e0777c
Jay Pipes <jaypipes@gmail.com>
2022-11-08 09:25:48 -0500

    consolidate hook code and remove wrong template
    We generally prefer to keep all custom hook code in a single file per
    resource manager, so consolidated that into pkg/resource/key/hooks.go.
    Also removed `templates/hooks/key/sdk_read_many_post_set_output.go.tpl`
    because the Key resource uses a READ_ONE not READ_MANY getter call
    (`DescribeKey` as opposed to `ListKeys`).
    
    Signed-off-by: Jay Pipes <jaypipes@gmail.com>

--- pkg/resource/key/hooks.go
+++ pkg/resource/key/hooks.go
@@ -22,6 +22,7 @@ import (
 	ackcompare "github.com/aws-controllers-k8s/runtime/pkg/compare"
 	ackerr "github.com/aws-controllers-k8s/runtime/pkg/errors"
 	ackrtlog "github.com/aws-controllers-k8s/runtime/pkg/runtime/log"
+	svcsdk "github.com/aws/aws-sdk-go/service/kms"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	svcapitypes "github.com/aws-controllers-k8s/kms-controller/apis/v1alpha1"
@@ -94,4 +95,86 @@ func (rm *resourceManager) customUpdate(
 	}
 	rm.setStatusDefaults(updatedRes.ko)
 	return updatedRes, nil
+}
+
+func (rm *resourceManager) updateKeyRotation(ctx context.Context, r *resource) (err error) {
+	rlog := ackrtlog.FromContext(ctx)
+	exit := rlog.Trace("rm.updateKeyRotation")
+
+	defer func() {
+		exit(err)
+	}()
+
+	keyRotationStatus, err := rm.getKeyRotationStatus(r)
+	if err != nil {
+		return err
+	}
+
+	// sanity check for response from key rotation status
+	if keyRotationStatus == nil || keyRotationStatus.KeyRotationEnabled == nil {
+		return nil
+	}
+
+	if r.ko.Spec.EnableKeyRotation == nil {
+		// check if current status of key is enabled
+		// if yes, then only disable, or skip and return
+		if *keyRotationStatus.KeyRotationEnabled {
+			return rm.disableKeyRotation(r.ko.Status.KeyID)
+		}
+		return nil
+	}
+
+	// Check if desired state and actual state has any difference
+	if *keyRotationStatus.KeyRotationEnabled != *r.ko.Spec.EnableKeyRotation {
+		switch *r.ko.Spec.EnableKeyRotation {
+		case true:
+			return rm.enableKeyRotation(r.ko.Status.KeyID)
+		case false:
+			return rm.disableKeyRotation(r.ko.Status.KeyID)
+		}
+	}
+
+	return nil
+}
+
+// get key rotation status at the kms key
+func (rm *resourceManager) getKeyRotationStatus(r *resource) (*svcsdk.GetKeyRotationStatusOutput, error) {
+	keyRotationInput := svcsdk.GetKeyRotationStatusInput{
+		KeyId: r.ko.Status.KeyID,
+	}
+	resp, err := rm.sdkapi.GetKeyRotationStatus(&keyRotationInput)
+	rm.metrics.RecordAPICall("GET", "GetKeyRotationStatus", err)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+// enable key rotation on the kms key
+func (rm *resourceManager) enableKeyRotation(keyId *string) error {
+	enableKeyRotationInput := svcsdk.EnableKeyRotationInput{
+		KeyId: keyId,
+	}
+	_, err := rm.sdkapi.EnableKeyRotation(&enableKeyRotationInput)
+	rm.metrics.RecordAPICall("UPDATE", "EnableKeyRotation", err)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// disable key rotation on the kms key
+func (rm *resourceManager) disableKeyRotation(keyId *string) error {
+	disableKeyRotationInput := svcsdk.DisableKeyRotationInput{
+		KeyId: keyId,
+	}
+	_, err := rm.sdkapi.DisableKeyRotation(&disableKeyRotationInput)
+	rm.metrics.RecordAPICall("UPDATE", "DisableKeyRotation", err)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- templates/hooks/key/sdk_read_many_post_set_output.go.tpl
+++
@@ -1,3 +0,0 @@
-if err := rm.setResourceAdditionalFields(ctx, ko); err != nil {
-    return nil, err
-}
