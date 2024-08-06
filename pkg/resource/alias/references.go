ec6df8b0ff4baaeb3f6c48804a0dcfe719c1cd0d
ci-robot <ci-robot@k8s.io>
2024-08-06 02:49:18 +0000

    Update to ACK runtime `v0.36.0`, code-generator `v0.36.0`

--- pkg/resource/alias/references.go
+++ pkg/resource/alias/references.go
@@ -56,12 +56,11 @@ func (rm *resourceManager) ResolveReferences(
 	apiReader client.Reader,
 	res acktypes.AWSResource,
 ) (acktypes.AWSResource, bool, error) {
-	namespace := res.MetaObject().GetNamespace()
 	ko := rm.concreteResource(res).ko
 
 	resourceHasReferences := false
 	err := validateReferenceFields(ko)
-	if fieldHasReferences, err := rm.resolveReferenceForTargetKeyID(ctx, apiReader, namespace, ko); err != nil {
+	if fieldHasReferences, err := rm.resolveReferenceForTargetKeyID(ctx, apiReader, ko); err != nil {
 		return &resource{ko}, (resourceHasReferences || fieldHasReferences), err
 	} else {
 		resourceHasReferences = resourceHasReferences || fieldHasReferences
@@ -90,7 +89,6 @@ func validateReferenceFields(ko *svcapitypes.Alias) error {
 func (rm *resourceManager) resolveReferenceForTargetKeyID(
 	ctx context.Context,
 	apiReader client.Reader,
-	namespace string,
 	ko *svcapitypes.Alias,
 ) (hasReferences bool, err error) {
 	if ko.Spec.TargetKeyRef != nil && ko.Spec.TargetKeyRef.From != nil {
@@ -98,6 +96,10 @@ func (rm *resourceManager) resolveReferenceForTargetKeyID(
 		arr := ko.Spec.TargetKeyRef.From
 		if arr.Name == nil || *arr.Name == "" {
 			return hasReferences, fmt.Errorf("provided resource reference is nil or empty: TargetKeyRef")
+		}
+		namespace := ko.ObjectMeta.GetNamespace()
+		if arr.Namespace != nil && *arr.Namespace != "" {
+			namespace = *arr.Namespace
 		}
 		obj := &svcapitypes.Key{}
 		if err := getReferencedResourceState_Key(ctx, apiReader, obj, *arr.Name, namespace); err != nil {
